1e23247a2e055ed6e3f773a1ce9782e77067525d
Marcin <krzyzaniak.marcin95@gmail.com>
2018-03-19 11:59:50 +0100

    get points on image edges

--- edges.py
+++ edges.py
@@ -8,11 +8,6 @@ import matplotlib as mpl
 import scipy.ndimage as ndimage
 from skimage import data, io, filters
 
-# get edges 
-img = io.imread('sets/set7/0.png')>127
-img_dilatation = ndimage.binary_dilation(img)
-diff = img_dilatation != img
-
 def removeBlankRows(image):
     x_begin = -1
     x_end = -1
@@ -34,8 +29,33 @@ def removeBlankRowsAndColumns(image):
     image = removeBlankRows(image)
     return image
 
+def getPointsOnEdges(image):
+    result = []
+    for i in range(len(image[0])):
+        if image[0][i]:
+            result.append([i,0])
+    for i in range(len(image[0])):
+        if image[len(image)-1][i]:
+            result.append([i,len(image)])
+    image = np.rot90(image, 3)
+    for i in range(len(image[0])):
+        if image[0][i]:
+            result.append([0,len(image[0])-i])
+    for i in range(len(image[0])):
+        if image[len(image)-1][i]:
+            result.append([len(image),len(image[0])-i])
+    return result
+
+# get edges 
+img = io.imread('sets/set7/0.png')>127
+img_dilatation = ndimage.binary_dilation(img)
+diff = img_dilatation != img
+
 #remove blank lines from image
 diff = removeBlankRowsAndColumns(diff)
+
+#get points on image edges
+points = getPointsOnEdges(diff)
 
 
 diff = diff.astype(np.int8)
